984d1b964900b3a823cd60cb2430291a5bf569d0
Plain Jane <jane99hsu@gmail.com>
2020-12-31 14:07:30 -0800

    added mobile responsiveness to initial layout

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,7 +5,7 @@ function Nav() {
         <nav>
             <p class="nav-element">About Me</p>
             <p class="nav-element">Portfolio</p>
-            <p class="nav-element">Contact Me</p>
+            <p class="nav-element">Contact</p>
             <p class="nav-element">Resume</p>
         </nav>
     )
