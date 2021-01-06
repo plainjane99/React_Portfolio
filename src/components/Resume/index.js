0ae041e24169ecaeca72a9ec75b09724104dae2a
Plain Jane <jane99hsu@gmail.com>
2021-01-05 22:14:08 -0800

    added resume content

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 function Resume() {
     return (
-        <section className="content-section resume-section">Some Content Here</section>
+        <section className="content-section resume-section">
+            <div className="section-wrapper">
+                <ul className="section">
+                    <li className="section-title">Front End Proficiencies</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaSCript</li>
+                    <li>JQuery</li>
+                    <li>Bootstrap</li>
+                    <li>React</li>
+                    <li>Responsive Design</li>
+                </ul>
+                <ul className="section">
+                    <li className="section-title">Back End Proficiencies</li>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL</li>
+                    <li>Sequelize</li>
+                    <li>MongoDB</li>
+                </ul>
+            </div>
+
+        </section>
     )
 }
 
