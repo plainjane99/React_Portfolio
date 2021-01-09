8d28ab2b777e6f5fe2a8b602f54c82f720199973
Plain Jane <jane99hsu@gmail.com>
2021-01-08 16:47:06 -0800

    added 3rd footer link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
             <div className="footer-wrapper">
                 <a href="https://github.com/plainjane99/" target="_blank" rel="noreferrer" className="footer-element">GitHub</a>
                 <a href="https://www.linkedin.com/in/jane-hsu-7329b497/" target="_blank" rel="noreferrer" className="footer-element">LinkedIn</a>
-                <p target="_blank" rel="noreferrer" className="footer-element">3rd Party</p>
+                <a href="https://stackoverflow.com/users/14031426/plainjane99" target="_blank" rel="noreferrer" className="footer-element">StackOverflow</a>
             </div>
         </footer>
     )
